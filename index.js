1d1fcb34dba4ee61b5d84ad4812c1767adbdbcae
nary406 <naryxyz3@gmail.com>
2024-04-08 23:55:36 +0530

    final

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 
 // Connect to MongoDB using Mongoose
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    family: 4,
+})
     .then(() => {
         console.log("Connected to MongoDB");
     })
